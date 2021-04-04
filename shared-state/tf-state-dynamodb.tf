8c178c4a67a50d351cf63cbbe3808c6209eefdcb
emgperez <em.gonzalez.perez@gmail.com>
2021-04-04 16:38:42 +0200

    Initial commit of files to save shared state

---
+++ shared-state/tf-state-dynamodb.tf
@@ -0,0 +1,27 @@
+# Two DynamoDB tables (meaning two resources defined), one for the VPC state lock and another for the cluster resources state lock
+resource "aws_dynamodb_table" "clusters_dynamodb_tf_state_lock" {
+  name = "${var.clusters_name_prefix}-terraform-state-lock-dynamodb"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
+
+resource "aws_dynamodb_table" "clusters_vpc_dynamodb_tf_state_lock" {
+
+  name = "${var.clusters_name_prefix}-vpc-terraform-state-lock-dynamodb"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
